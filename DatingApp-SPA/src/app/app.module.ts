fe7abf962eb970f307ee687c0ec58f163dd3a70e
Rajas Yellapurkar <rajasyellapurkargit@gmail.com>
2020-03-08 09:56:59 +0530

    End of Section 7

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import { BsDropdownModule } from 'ngx-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -10,9 +13,10 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 
-import { BsDropdownModule } from 'ngx-bootstrap';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { appRoutes } from './routes';
 
 
 @NgModule({
@@ -21,14 +25,17 @@ import { MemberListComponent } from './member-list/member-list.component';
       NavComponent,
       HomeComponent,
       RegisterComponent,
-      MemberListComponent
+      MemberListComponent,
+      ListsComponent,
+      MessagesComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       FormsModule,
       BrowserAnimationsModule,
-      BsDropdownModule.forRoot()
+      BsDropdownModule.forRoot(),
+      RouterModule.forRoot(appRoutes)
    ],
    providers: [
       AuthService,
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,7 +12,7 @@ export class NavComponent implements OnInit {
 
   model:any ={};
 
-  constructor(public authService:AuthService, private alertify:AlertifyService) { }
+  constructor(public authService:AuthService, private alertify:AlertifyService, private router:Router) { }
 
   ngOnInit() {
   }
@@ -24,6 +25,9 @@ export class NavComponent implements OnInit {
       error => {
         console.log(error);
         this.alertify.error('Failed to login: '+ error);
+      },
+      ()=>{
+          this.router.navigate(['/members']);
       }
     );
   }
@@ -35,6 +39,7 @@ export class NavComponent implements OnInit {
   logOut(){
     localStorage.removeItem('token');
     this.alertify.message('logged out');
+    this.router.navigate(['/home']);
   }
 
 }
